acd79960fda254541b7b43243afe76b475dad17a
Peron Stevens <PeronStevens@outlook.com>
2019-12-05 21:38:26 -0500

    install tailwind cause utility css is superior

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,6 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
 
 window.Vue = require('vue');
 
@@ -19,7 +18,8 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('show-viewer-component', require('./components/ShowViewerComponent.vue').default);
+Vue.component('show-component', require('./components/ShowComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
---
+++ resources/js/components/ShowViewerComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex mb-4">
+        <div class="w-1/2 bg-gray-400 h-12">
+            Testing
+        </div>
+        <div class="w-1/2 bg-gray-500 h-12">
+            oirngerino
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
